816653423d094042f295e78836fcd9c48a544e02
Jasdeep Singh <j.singhhh917@gmail.com>
2025-01-26 18:01:19 +1100

    creating object

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ lotto.pop();
 
 shoppingList.unshift('soda');
 lotto.shift();
+
+
+//////////////////////////////////////////////////
+
+const fitBitData = {
+  totalSteps: 308727,
+  totalMiles: 211.7,
+  avgCalorieBurn: 5755,
+  workoutsThisWeek: '5 of 7',
+  avgGoodSleep: '2:13'
+}
