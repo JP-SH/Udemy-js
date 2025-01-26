d8926ea7506d0b3c6016f10bbeafd68ed6d806e7
Jasdeep Singh <j.singhhh917@gmail.com>
2025-01-26 22:28:40 +1100

    perfect square loop

--- loop.js
+++ loop.js
@@ -1,6 +1,12 @@
 // simple for loop
-for  (let j = 1;
-  j <= 10;
-  j++) {
-  console.log('AYOOO',j);
-  }
+// for  (let j = 1;
+//   j <= 10;
+//   j++) {
+//   console.log('AYOOO',j);
+//   }
+
+
+// perfect square loop
+for (let square = 1; square <= 20; square++){
+  console.log(`${square} x ${square} ${square * square}`);
+}
