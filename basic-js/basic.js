c8439a1ba889924c9f49523db7ff132141ed959b
Jasdeep Singh <j.singhhh917@gmail.com>
2025-07-14 22:23:36 +1000

    basic js

---
+++ basic-js/basic.js
@@ -0,0 +1,21 @@
+// primitives-
+// numbers
+// string
+// boolean
+// null
+// undefined
+
+// Not a number (NaN)
+0 / 0
+
+// variables
+let best = 'mancity';
+// const variables value cannot be changed
+const treble = 'citeh';
+
+// post increment operator
+// this returns the current value of i and then increments
+i++
+// pre increment operator
+// this returns the value of i after it has been incremented
+++i
