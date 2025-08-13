c831331dbef6aa214f0ecaa79d5084fccb356f45
Jasdeep Singh <j.singhhh917@gmail.com>
2025-08-13 16:17:30 +1000

    array.js

--- basic-js/basic.js
+++ basic-js/basic.js
@@ -391,52 +391,6 @@ const highestScore = Math.max(...scores4);
 
 console.log(highestScore); // Output: 100
 
-// array
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5];
-
-// Combine using spread
-const combined = [...arr1, ...arr2];
-
-console.log(combined); // Output: [1, 2, 3, 4, 5]
-
-// rest params - it will collect all remaining arguments into an array
-function sum(...numbers) {
-  let total = 0;
-  for (let num of numbers) {
-    total += num;
-  }
-  return total;
-}
-
-// push and pop
-let fruits = ['apple', 'banana'];
-fruits.push('orange');
-
-// console.log(fruits);  Output: ['apple', 'banana', 'orange']
-let removedFruit = fruits.pop();
-
-// console.log(fruits);        Output: ['apple', 'banana']
-// console.log(removedFruit);  Output: 'orange'
-
-// shift and unshift
-let colourz = ['blue', 'green'];
-colourz.unshift('red');
-
-// console.log(colourz);  Output: ['red', 'blue', 'green']
-let removedColour = colourz.shift();
-
-console.log(colourz);       // Output: ['blue', 'green']
-console.log(removedColour); // Output: 'red'
-
-// destructuring array
-const colours = ['red', 'green', 'blue'];
-
-const [firstColour, secondColour, thirdColour] = colours;
-
-console.log(firstColour);  // 'red'
-console.log(secondColour); // 'green'
-console.log(thirdColour);  // 'blue'
 
 // destructuring object
 const car = {
@@ -459,20 +413,7 @@ const human2 = { name: 'John' };
 sayName(human2);
 // Output: Hello, John!
 
-// multi nested array
-const nestedArray = [
-  { name: "Jay", age: 25 },
-  [
-    { name: "Kay", age: 30 },
-    [
-      { name: "Kev", age: 35 },
-      { name: "Harry", age: 40 }
-    ]
-  ],
-  [
-    { name: "Ron", age: 28 }
-  ]
-];
+
 
 // filter
 let evens = [1, 2, 3, 4].filter(x => x % 2 === 0); // [2, 4]
