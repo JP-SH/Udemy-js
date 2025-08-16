9cc156487a7b71bf481fc3402102d7e11fa2e392
Jasdeep Singh <j.singhhh917@gmail.com>
2025-08-16 23:40:41 +1000

    prototype

---
+++ OOP/prototypes/app.js
@@ -0,0 +1,13 @@
+//String.prototype is a "template object" for every single string.
+String.prototype.yell = function() {
+	return `OMG!!! ${this.toUpperCase()}!!!!! AGHGHGHG!`;
+};
+
+'bees'.yell(); //"OMG!!! BEES!!!!! AGHGHGHG!"
+
+//We can overwrite an existing Array method like pop (not a good idea):
+Array.prototype.pop = function() {
+	return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!';
+};
+const nums = [ 6, 7, 8, 9 ];
+nums.pop(); // "SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF!"
